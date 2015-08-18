18763520e161c85d04ddd8dd4f80468cfd5b4f76
Elmar Hinz <t3elmar@gmail.com>
2015-08-18 18:23:48 +0200

    Bootstrapping up to ansible installation playbook

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Alpha
+
+[The Vision](TheBigPlan.md)
+
+Warning: This is alpha and you may lose data! 
+
+# Installation
+
+```bash
+git clone https://github.com/elmar-hinz/OSX.Lord.git Lord_Installer
+Lord_Installer/install.rb
+rm -rf Lord_Installer
+```
+
+This will install Lord into `~/Library/Lord/`.
+
 
--- install.rb
+++ install.rb
@@ -1,19 +1,28 @@
 #! /usr/bin/env ruby
 
-# Set env LORD_HOME if not existant 
-ENV['LORD_HOME'] = ENV['HOME'] + '/Library/Lord' unless(ENV.key?('LORD_HOME')) 
+filename = File.dirname(__FILE__) + '/bootstrap.rc'
+puts '* source bootstrapping environment settings from: ' + filename
+ENV.replace(eval(`bash -c 'source #{filename} && ruby -e "p ENV"'`))
 
-# Clone Lord
-if(false) 
+puts '* Ensuring Lord is cloned into ' + ENV['LORD_HOME'] 
+unless(Dir.exists?(ENV['LORD_HOME'])) 
     cmd = 'git clone https://github.com/elmar-hinz/OSX.Lord.git ' + ENV['LORD_HOME'] 
-    puts cmd
+    puts '* ' + cmd
     system(cmd)
+end
+
+puts '* Ensuring Homebrew is available ' 
+unless(`brew --version`) 
+    cmd = 'git clone https://github.com/Homebrew/homebrew.git ' + ENV['HOMEBREW_PREFIX'] 
+    puts '* ' + cmd
+    system(cmd)
+    Dir.mkdir(ENV['HOMEBREW_TEMP'])
 end
 
 puts '* Ensuring Ansible is installed'
 unless(`ansible --version`) 
     cmd = 'brew install ansible'
-    puts cmd
+    puts '* ' + cmd
     system(cmd)
 end
 
